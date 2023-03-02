8e888a7da3cf113cb79f612bd9e8ca70cf96eac8
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-03-02 23:51:38 +0100

    Initial commit

---
+++ src/main/java/com/ciosmak/automotivepartner/AutomotivePartnerApplication.java
@@ -0,0 +1,15 @@
+package com.ciosmak.automotivepartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class AutomotivePartnerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AutomotivePartnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ciosmak/automotivepartner/AutomotivePartnerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ciosmak.automotivepartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotivePartnerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
