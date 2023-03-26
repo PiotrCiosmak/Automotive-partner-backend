40abf8381b9a92eef197ae7ed2b825ba090c594e
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-03-26 15:49:05 +0200

    basic info in swagger view added

--- src/main/java/com/ciosmak/automotivepartner/AutomotivePartnerApplication.java
+++ src/main/java/com/ciosmak/automotivepartner/AutomotivePartnerApplication.java
@@ -1,9 +1,22 @@
 package com.ciosmak.automotivepartner;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Automotive Partner",
+                version = "0.0.1",
+                contact = @Contact(
+                        name = "Piotr Ciosmak",
+                        email = "piotrciosmak2001@gmail.com"
+                )
+        )
+)
 public class AutomotivePartnerApplication
 {
 
