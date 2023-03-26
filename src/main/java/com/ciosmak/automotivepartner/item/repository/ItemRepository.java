3e712cdbbbc73ac165b507ffa5846458e8b4c530
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-03-26 19:04:37 +0200

    ItemRepository is now a interface that extends JpaRepository

--- src/main/java/com/ciosmak/automotivepartner/item/repository/ItemRepository.java
+++ src/main/java/com/ciosmak/automotivepartner/item/repository/ItemRepository.java
@@ -1,49 +1,10 @@
 package com.ciosmak.automotivepartner.item.repository;
 
 import com.ciosmak.automotivepartner.item.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
-
 @Repository
-public class ItemRepository
+public interface ItemRepository extends JpaRepository<Item, Long>
 {
-    protected final Map<Long, Item> map = new HashMap<>();
-    protected long counter = 1;
-
-    public Item save(Item entity)
-    {
-        setData(entity);
-        return entity;
-    }
-
-    private Item setData(Item entity)
-    {
-        if (entity.getId() != null)
-        {
-            map.put(entity.getId(), entity);
-        }
-        else
-        {
-            entity.setId(counter);
-            map.put(counter, entity);
-            counter++;
-        }
-        return entity;
-    }
-
-    public Optional<Item> findById(Long id)
-    {
-        return Optional.ofNullable(map.get(id));
-    }
-
-    public List<Item> findAll()
-    {
-        return new ArrayList<>(map.values());
-    }
-
-    public void deleteById(Long id)
-    {
-        map.remove(id);
-    }
 }
