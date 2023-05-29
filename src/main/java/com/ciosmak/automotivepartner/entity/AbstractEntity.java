fbc9a8752d2a50dcf83b7271b70311fe3196219a
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-05-30 00:57:48 +0200

    AbstractEntity added

---
+++ src/main/java/com/ciosmak/automotivepartner/entity/AbstractEntity.java
@@ -0,0 +1,16 @@
+package com.ciosmak.automotivepartner.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@MappedSuperclass
+public class AbstractEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+}
