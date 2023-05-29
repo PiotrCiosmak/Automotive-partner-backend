b226f1cd368c365ef2c8cf9c6bc9105169b09bfb
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-05-30 00:41:25 +0200

    Image entity done

---
+++ src/main/java/com/ciosmak/automotivepartner/image/Image.java
@@ -0,0 +1,22 @@
+package com.ciosmak.automotivepartner.image;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "images")
+public class Image
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+}
