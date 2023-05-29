10dc31569a8ff685f0415ef1e0ea8bbac4ba99f1
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-05-29 23:59:37 +0200

    availability entity done

--- src/main/java/com/ciosmak/automotivepartner/availability/Availability.java
+++ src/main/java/com/ciosmak/automotivepartner/availability/Availability.java
@@ -1,15 +1,12 @@
 package com.ciosmak.automotivepartner.availability;
 
-import com.ciosmak.automotivepartner.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Getter
 @Setter
@@ -23,11 +20,7 @@ public class Availability
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private State state;
+    private Type type;
 
     private Date date;
-
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinTable(name = "availability_users", joinColumns = {@JoinColumn(name = "availability_id")}, inverseJoinColumns = {@JoinColumn(name = "user_id")})
-    private List<User> users = new ArrayList<>();
 }
