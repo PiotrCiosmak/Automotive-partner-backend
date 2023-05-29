45cb2efda771bb7870059a1d6a8baa4589d81394
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-05-30 00:38:14 +0200

    ImageType entity updated

--- src/main/java/com/ciosmak/automotivepartner/image/ImageType.java
+++ src/main/java/com/ciosmak/automotivepartner/image/ImageType.java
@@ -21,7 +21,7 @@ public class ImageType
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String type;
+    private String type;// {SHIFT_START, SHIFT_END, SHIFT_START_MILEAGE, SHIFT_END_MILEAGE, RECEIPT, CAR_DAMAGE, STATEMENT}
 
     @OneToMany(mappedBy = "imagesTypes", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Image> images = new ArrayList<>();
