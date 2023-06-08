1178d8628040b92de174a3651da26a6f357a9e82
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-06-08 18:15:47 +0200

    some query's added

--- src/main/java/com/ciosmak/automotivepartner/user/repository/UserRepository.java
+++ src/main/java/com/ciosmak/automotivepartner/user/repository/UserRepository.java
@@ -2,12 +2,22 @@ package com.ciosmak.automotivepartner.user.repository;
 
 import com.ciosmak.automotivepartner.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>
 {
     Optional<User> findByEmail(String email);
+
+    List<User> findAllByBlockedFalse();
+
+    List<User> findAllByBlockedTrue();
+
+    @Query("SELECT CASE WHEN u.blocked = true THEN true ELSE false END FROM User u WHERE u.id = :id")
+    Boolean isUserBlocked(@Param("id") Long id);
 }
