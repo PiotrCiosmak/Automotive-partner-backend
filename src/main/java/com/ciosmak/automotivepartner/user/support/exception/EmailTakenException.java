907d0cf276e241833d5b406a9a49c90ef1b584b6
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-06-10 12:09:00 +0200

    message changed

--- src/main/java/com/ciosmak/automotivepartner/user/support/exception/EmailTakenException.java
+++ src/main/java/com/ciosmak/automotivepartner/user/support/exception/EmailTakenException.java
@@ -4,6 +4,6 @@ public class EmailTakenException extends RuntimeException
 {
     public EmailTakenException()
     {
-        super("Podany email jest już przypisany do istniejącego konta");
+        super("Podany email jest już zajęty");
     }
 }
