8ed984ee973fac04559e40d331a81cf5a821bb7d
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-08-14 22:24:13 +0200

    response and request added

--- src/main/java/com/ciosmak/automotivepartner/settlement/api/request/SettlementRequest.java
+++ src/main/java/com/ciosmak/automotivepartner/settlement/api/request/SettlementRequest.java
@@ -1,5 +1,31 @@
 package com.ciosmak.automotivepartner.settlement.api.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
 public class SettlementRequest
 {
+    private final LocalDate date;
+    private final BigDecimal netProfit;
+    private final BigDecimal factor;
+    private final BigDecimal tips;
+    private final BigDecimal penalties;
+    private final BigDecimal finalProfit;
+    private final Boolean bugReported;
+
+    @JsonCreator
+    public SettlementRequest(LocalDate date, BigDecimal netProfit, BigDecimal factor, BigDecimal tips, BigDecimal penalties, BigDecimal finalProfit, Boolean bugReported)
+    {
+        this.date = date;
+        this.netProfit = netProfit;
+        this.factor = factor;
+        this.tips = tips;
+        this.penalties = penalties;
+        this.finalProfit = finalProfit;
+        this.bugReported = bugReported;
+    }
 }
--- src/main/java/com/ciosmak/automotivepartner/settlement/api/response/SettlementResponse.java
+++ src/main/java/com/ciosmak/automotivepartner/settlement/api/response/SettlementResponse.java
@@ -1,5 +1,21 @@
 package com.ciosmak.automotivepartner.settlement.api.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Getter
 public class SettlementResponse
 {
+    private final Long id;
+    private final LocalDate date;
+    private final BigDecimal netProfit;
+    private final BigDecimal factor;
+    private final BigDecimal tips;
+    private final BigDecimal penalties;
+    private final BigDecimal finalProfit;
+    private final Boolean bugReported;
 }
