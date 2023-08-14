8090b04eb472af4ae75f6d89b8c7fee6bad89c55
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-08-14 22:26:22 +0200

    refactor of user repository

--- src/main/java/com/ciosmak/automotivepartner/user/repository/UserRepository.java
+++ src/main/java/com/ciosmak/automotivepartner/user/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.ciosmak.automotivepartner.user.repository;
 
 import com.ciosmak.automotivepartner.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -22,14 +21,6 @@ public interface UserRepository extends JpaRepository<User, Long>
     @Query("SELECT u.blocked FROM User u WHERE u.id = :id")
     boolean isBlocked(@Param("id") Long id);
 
-    @Modifying
-    @Query("UPDATE User u SET u.blocked = :isBlocked WHERE u.id = :id")
-    void setBlocked(@Param("id") Long id, Boolean isBlocked);
-
     @Query("SELECT u.role FROM User u WHERE u.id = :id")
     String getRole(@Param("id") Long id);
-
-    @Modifying
-    @Query("UPDATE User u SET u.role = :role WHERE u.id = :id")
-    void setRole(@Param("id") Long id, String role);
 }
