1e9cf85302a08e0bad4b054df6c3fe50df2f73c6
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-09-17 15:24:01 +0200

    photos done

--- src/main/java/com/ciosmak/automotivepartner/photo/domain/Photo.java
+++ src/main/java/com/ciosmak/automotivepartner/photo/domain/Photo.java
@@ -1,6 +1,6 @@
-package com.ciosmak.automotivepartner.image;
+package com.ciosmak.automotivepartner.photo.domain;
 
-import com.ciosmak.automotivepartner.entity.AbstractEntity;
+import com.ciosmak.automotivepartner.shared.entity.AbstractEntity;
 import com.ciosmak.automotivepartner.shift.domain.Shift;
 import jakarta.persistence.*;
 import lombok.*;
@@ -13,18 +13,25 @@ import lombok.*;
 @ToString(onlyExplicitlyIncluded = true)
 
 @Entity
-@Table(name = "images")
-public class Image extends AbstractEntity
+@Table(name = "photos")
+public class Photo extends AbstractEntity
 {
     @ToString.Include
     @Column(name = "url", nullable = false)
     private String url;
 
+    // "SHIFT_START"
+    // "SHIFT_END"
+    // "SHIFT_START_MILEAGE"
+    // "SHIFT_END_MILEAGE"
+    // "INVOICE"
+    // "CAR_DAMAGE"
+    // "STATEMENT"
+    @ToString.Include
+    @Column(name = "type", nullable = false)
+    private String type;
+
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "shift_id")
     private Shift shift;
-
-    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(name = "image_type_id")
-    private ImageType imageType;
 }
---
+++ src/main/java/com/ciosmak/automotivepartner/photo/repository/PhotoRepository.java
@@ -0,0 +1,13 @@
+package com.ciosmak.automotivepartner.photo.repository;
+
+import com.ciosmak.automotivepartner.photo.domain.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Long>
+{
+    List<Photo> findByShiftIdAndType(Long shiftId, String type);
+}
