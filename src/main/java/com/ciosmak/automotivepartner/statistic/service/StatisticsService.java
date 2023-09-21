4ebffa7b6b954832c88eb5a50abcba133cdd0a1f
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-09-22 01:39:55 +0200

    saving statistics changes fixed

--- src/main/java/com/ciosmak/automotivepartner/statistic/service/StatisticsService.java
+++ src/main/java/com/ciosmak/automotivepartner/statistic/service/StatisticsService.java
@@ -16,14 +16,12 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Year;
-import java.util.Map;
 import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class StatisticsService
 {
-    //TODO po każdej zakończonej zmianie na bazie roku i miesiac updatowany jest rekord w tabeli (dodawane są przejchane km, spalony gaz i pb)
     private final UserRepository userRepository;
     private final StatisticsRepository statisticsRepository;
     private final StatisticsMapper statisticsMapper;
@@ -48,32 +46,27 @@ public class StatisticsService
         Statistics statistics = statisticsRepository.findByUserIdAndDate(userId, adjustedDate).orElseThrow(StatisticsExceptionSupplier.incorrectDate(user.getFirstName(), user.getLastName(), date.getMonthValue(), date.getYear()));
 
         return statisticsMapper.toStatisticsResponse(statistics);
-
     }
 
     private LocalDate adjustDate(LocalDate date)
     {
         return date.withDayOfMonth(1);
     }
-
+    
     public YearStatisticsResponse findInSelectedYear(Year year)
     {
-        Map<String, BigDecimal> yearStatistics = statisticsRepository.sumValuesByYear(year.getValue());
+        Integer yearMileage = statisticsRepository.sumMileageByYear(year.getValue());
+        BigDecimal yearLpg = statisticsRepository.sumLpgByYear(year.getValue());
+        BigDecimal yearPetrol = statisticsRepository.sumPetrolByYear(year.getValue());
 
-        checkIfYearIsCorrect(yearStatistics, year);
+        checkIfYearIsCorrect(yearMileage, yearLpg, yearPetrol, year);
 
-        yearStatistics.putIfAbsent("mileage", BigDecimal.ZERO);
-
-        yearStatistics.putIfAbsent("lpg", BigDecimal.ZERO);
-
-        yearStatistics.putIfAbsent("lpg", BigDecimal.ZERO);
-
-        return statisticsMapper.toYearStatisticsResponse(yearStatistics);
+        return statisticsMapper.toYearStatisticsResponse(yearMileage, yearLpg, yearPetrol);
     }
 
-    private void checkIfYearIsCorrect(Map<String, BigDecimal> yearStatistics, Year year)
+    private void checkIfYearIsCorrect(Integer yearMileage, BigDecimal yearLpg, BigDecimal yearPetrol, Year year)
     {
-        if (yearStatistics.get("mileage") == null || yearStatistics.get("lpg") == null || yearStatistics.get("petrol") == null)
+        if (yearMileage == null || yearLpg == null || yearPetrol == null)
         {
             throw StatisticsExceptionSupplier.incorrectYear(year.getValue()).get();
         }
