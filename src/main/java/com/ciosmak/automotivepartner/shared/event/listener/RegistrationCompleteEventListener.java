00f96e1c29250ae214f762f09069e9de739b1550
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-10-10 22:09:01 +0200

    sending verification email is DONE!

--- src/main/java/com/ciosmak/automotivepartner/shared/event/listener/RegistrationCompleteEventListener.java
+++ src/main/java/com/ciosmak/automotivepartner/shared/event/listener/RegistrationCompleteEventListener.java
@@ -1,15 +1,20 @@
-package com.ciosmak.automotivepartner.user.support.event.listener;
+package com.ciosmak.automotivepartner.shared.event.listener;
 
+import com.ciosmak.automotivepartner.shared.event.RegistrationCompleteEvent;
 import com.ciosmak.automotivepartner.token.verification.api.request.VerificationTokenRequest;
 import com.ciosmak.automotivepartner.token.verification.api.response.VerificationTokenResponse;
 import com.ciosmak.automotivepartner.token.verification.service.VerificationTokenService;
 import com.ciosmak.automotivepartner.user.domain.User;
-import com.ciosmak.automotivepartner.user.support.event.RegistrationCompleteEvent;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -19,6 +24,7 @@ import java.util.UUID;
 public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent>
 {
     private final VerificationTokenService verificationTokenService;
+    private final JavaMailSender mailSender;
 
     @Override
     public void onApplicationEvent(RegistrationCompleteEvent event)
@@ -30,8 +36,15 @@ public class RegistrationCompleteEventListener implements ApplicationListener<Re
 
         String url = event.getApplicationUrl() + "/api/tokens/verification/verify_email?token=" + verificationTokenResponse.getToken();
 
-        //TODO wysyłać maila
-        log.info("Click the link to complete your registration: {}", url);
+        //TODO zmienić te błedy na moje własne
+        try
+        {
+            sendVerificationEmail(url, user);
+        }
+        catch (MessagingException | UnsupportedEncodingException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     private VerificationTokenRequest generateNewVerificationToken(User user)
@@ -45,5 +58,23 @@ public class RegistrationCompleteEventListener implements ApplicationListener<Re
 
         LocalDateTime now = LocalDateTime.now();
         return now.plusMinutes(default_expiration_time);
+    }
+
+    private void sendVerificationEmail(String url, User user) throws MessagingException, UnsupportedEncodingException
+    {
+        String subject = "Weryfikacja adresu email";
+        String senderName = "AutomotivePartner";
+        String mailContent = "<p> Cześć " + user.getFirstName() + ", </p>" +
+                "<p>Dziękujemy za rejestrację w naszym serwisie.</p>" +
+                "<p>Proszę kliknąć poniższy link, aby ukończyć proces rejestracji.</p>" +
+                "<a href=\"" + url + "\">Zweryfikuj swój adres e-mail, aby aktywować swoje konto.</a>" +
+                "<p>Dziękujemy<br>AutomotivePartner";
+        MimeMessage message = mailSender.createMimeMessage();
+        var messageHelper = new MimeMessageHelper(message);
+        messageHelper.setFrom("automotive.partner.biz@gmail.com", senderName);
+        messageHelper.setTo(user.getEmail());
+        messageHelper.setSubject(subject);
+        messageHelper.setText(mailContent, true);
+        mailSender.send(message);
     }
 }
