4618fe8337b8d32f70bbdb36a807d0beeffe6b59
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-10-10 19:23:11 +0200

    verification token domain added

---
+++ src/main/java/com/ciosmak/automotivepartner/token/verification/domain/VerificationToken.java
@@ -0,0 +1,31 @@
+package com.ciosmak.automotivepartner.token.verification.domain;
+
+import com.ciosmak.automotivepartner.shared.entity.AbstractEntity;
+import com.ciosmak.automotivepartner.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@ToString(onlyExplicitlyIncluded = true)
+
+@Entity
+@Table(name = "tokens")
+public class VerificationToken extends AbstractEntity
+{
+    @ToString.Include
+    @Column(name = "token", nullable = false)
+    private String token;
+
+    @Column(name = "expiration_time", columnDefinition = "TIMESTAMP ", nullable = false)
+    private LocalDateTime expirationTime;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
