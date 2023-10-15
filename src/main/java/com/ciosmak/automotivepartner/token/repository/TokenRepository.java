d4fa2be6e83a31265a03de096a6327905b28a4c1
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-10-15 15:45:58 +0200

    two new query added

--- src/main/java/com/ciosmak/automotivepartner/token/repository/TokenRepository.java
+++ src/main/java/com/ciosmak/automotivepartner/token/repository/TokenRepository.java
@@ -2,13 +2,19 @@ package com.ciosmak.automotivepartner.token.repository;
 
 import com.ciosmak.automotivepartner.token.domain.Token;
 import com.ciosmak.automotivepartner.token.support.TokenType;
+import com.ciosmak.automotivepartner.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long>
 {
     Optional<Token> findByTokenAndType(String token, TokenType type);
+
+    Optional<Token> findByUserAndTypeAndExpirationTimeAfter(User user, TokenType type, LocalDateTime expirationTime);
+
+    Optional<Token> findByUserAndTypeAndExpirationTimeBefore(User user, TokenType type, LocalDateTime expirationTime);
 }
