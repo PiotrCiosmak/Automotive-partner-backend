df93b430550166c78eb5672b637c378a3586786f
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-11-22 00:48:43 +0100

    added

--- src/main/java/com/ciosmak/automotivepartner/shift/repository/ShiftRepository.java
+++ src/main/java/com/ciosmak/automotivepartner/shift/repository/ShiftRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,11 +28,13 @@ public interface ShiftRepository extends JpaRepository<Shift, Long>
     @Query("SELECT SUM(s.lpg) FROM Shift s WHERE YEAR(s.date) = :year AND MONTH(s.date) = :month AND s.user.id = :userId")
     Optional<BigDecimal> getTotalLpgByUserAndYearAndMonth(Integer year, Integer month, Long userId);
 
-    List<Shift> findByIsCarAvailableFalseAndDateGreaterThanEqual(LocalDate date);
+    @Query("SELECT s FROM Shift s WHERE s.isCarAvailable=false  AND s.date > :date OR (s.date = :date AND ((HOUR(:date) < 6) OR (HOUR(:date) >= 6 AND HOUR(:date) < 18 AND s.type = 1)))")
+    List<Shift> findByIsCarAvailableFalseAndDateGreaterThanEqual(LocalDateTime date);
+    //TODO TEST SPRAWDZIĆ CZY DZIAŁA (SWAGGER FORMAT DATY, jesli nie to dać LocalDate i LocalTime osobno)
 
-    @Query("SELECT s FROM Shift s WHERE s.isStarted = false AND s.isDone = false AND s.date >= CURRENT_DATE AND s.car.id = :carId")
+    @Query("SELECT s FROM Shift s WHERE s.isStarted = false AND s.isDone = false AND s.car.id = :carId AND s.date > CURRENT_DATE OR (s.date = CURRENT_DATE AND ((HOUR(CURRENT_TIME) < 6) OR (HOUR(CURRENT_TIME) >= 6 AND HOUR(CURRENT_TIME) < 18 AND s.type = 1)))")
     List<Shift> findUnstartedAndUndoneShifts(Long carId);
 
-    @Query("SELECT s FROM Shift s WHERE s.isCarAvailable = false AND s.date >= CURRENT_DATE")
-    List<Shift> findShiftsForCarAndFutureDate(Long carId);
+    @Query("SELECT s FROM Shift s WHERE s.isCarAvailable = false AND s.date > CURRENT_DATE OR (s.date = CURRENT_DATE AND ((HOUR(CURRENT_TIME) < 6) OR (HOUR(CURRENT_TIME) >= 6 AND HOUR(CURRENT_TIME) < 18 AND s.type = 1)))")
+    List<Shift> findShiftsForCarAndDateFuture(Long carId);
 }
