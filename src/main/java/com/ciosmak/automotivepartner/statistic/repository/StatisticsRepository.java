bca590009648477a836346a20c1c185686667cde
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-11-30 00:49:47 +0100

    fix in repo

--- src/main/java/com/ciosmak/automotivepartner/statistic/repository/StatisticsRepository.java
+++ src/main/java/com/ciosmak/automotivepartner/statistic/repository/StatisticsRepository.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StatisticsRepository extends JpaRepository<Statistics, Long>
 {
-    Optional<Statistics> findByUserId(Long userId);
+    List<Statistics> findByUserId(Long userId);
 
     Optional<Statistics> findByUserIdAndDate(Long userId, LocalDate date);
 
