5b160037054557829ff52f39648c86f6dce8399c
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-12-03 16:28:24 +0100

    tests upgraded

--- src/test/java/com/ciosmak/automotivepartner/repository/EmailRepositoryTest.java
+++ src/test/java/com/ciosmak/automotivepartner/repository/EmailRepositoryTest.java
@@ -3,55 +3,61 @@ package com.ciosmak.automotivepartner.repository;
 import com.ciosmak.automotivepartner.email.domain.Email;
 import com.ciosmak.automotivepartner.email.repository.EmailRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class EmailRepositoryTest
 {
+    private static int numberOfEmails;
     @Autowired
     private EmailRepository emailRepository;
 
-    private static Email email;
-
-    @BeforeAll
-    public static void setUp()
+    @BeforeEach
+    public void setUp()
     {
-        email = Email.builder().email("test@example.com").build();
+        List<Email> emails = new ArrayList<>();
+        emails.add(Email.builder().email("test.a@example.com").build());
+        emailRepository.saveAll(emails);
+        numberOfEmails = emails.size();
     }
 
     @Test
     public void shouldReturnEmailWhenEmailIsInDatabase()
     {
-        Email savedEmail = emailRepository.save(email);
+        Email savedEmail = emailRepository.save(Email.builder().email("test.b@example.com").build());
 
         Assertions.assertThat(savedEmail).isNotNull();
+        Assertions.assertThat(savedEmail.getEmail()).isEqualTo("test.b@example.com");
         Assertions.assertThat(savedEmail.getId()).isGreaterThan(0);
     }
 
-    @Test
-    public void shouldFindEmailByEmailWhenEmailIsInDatabase()
+    @ParameterizedTest
+    @ValueSource(strings = {"test.a@example.com"})
+    public void shouldFindEmailByEmailWhenEmailIsInDatabase(String expectedEmail)
     {
-        emailRepository.save(email);
-
-        Optional<Email> foundEmail = emailRepository.findByEmail(email.getEmail());
+        Optional<Email> foundEmail = emailRepository.findByEmail(expectedEmail);
 
         Assertions.assertThat(foundEmail).isNotEmpty();
         Assertions.assertThat(foundEmail.get()).isNotNull();
-        Assertions.assertThat(foundEmail.get().getEmail()).isEqualTo("test@example.com");
+        Assertions.assertThat(foundEmail.get().getEmail()).isEqualTo(expectedEmail);
     }
 
     @Test
     public void shouldNotFindEmailByEmailWhenEmailIsNotInDatabase()
     {
-        Optional<Email> foundEmail = emailRepository.findByEmail(email.getEmail());
+        Optional<Email> foundEmail = emailRepository.findByEmail("nonexistsing@example.com");
 
         Assertions.assertThat(foundEmail).isEmpty();
     }
@@ -59,11 +65,14 @@ public class EmailRepositoryTest
     @Test
     public void shouldDeleteEmailWhenEmailIsInDatabase()
     {
-        emailRepository.save(email);
+        long firstId = emailRepository.findAll().get(0).getId();
+        long lastId = firstId + numberOfEmails;
+        for (long i = firstId; i < lastId; ++i)
+        {
+            emailRepository.deleteById(i);
+            Optional<Email> foundEmail = emailRepository.findById(i);
 
-        emailRepository.deleteById(email.getId());
-        Optional<Email> foundEmail = emailRepository.findById(email.getId());
-
-        Assertions.assertThat(foundEmail).isEmpty();
+            Assertions.assertThat(foundEmail).isEmpty();
+        }
     }
 }
